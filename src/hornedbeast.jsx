f7a080753e958a08f59ff5a86d225fd6234135fa
Qilin Xie <qxie@gicw.org>
2024-03-25 15:15:20 -0700

    adds lab requirement

---
+++ src/hornedbeast.jsx
@@ -0,0 +1,14 @@
+// HornedBeast.jsx
+import React from 'react';
+
+function HornedBeast({ title, imageUrl, description }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <img src={imageUrl} alt={title} title={title} />
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default HornedBeast;
