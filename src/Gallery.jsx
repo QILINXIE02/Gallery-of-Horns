340582ba63cb8a53387c8c4afd2d60101c2e355d
Qilin Xie <qxie@gicw.org>
2024-03-26 08:26:31 -0700

    renders gallery.jsx

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -12,8 +12,13 @@ function Gallery() {
       />
       <HornedBeast 
         title="Rhino Family" 
-        imageUrl="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" 
+        imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" 
         description="Parent rhino with two babies" 
+      />
+      <HornedBeast 
+        title="unicorn" 
+        imageUrl="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" 
+        description="Someone wearing a very silly unicorn head mask" 
       />
     </div>
   );
--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -1,12 +1,12 @@
-// HornedBeast.jsx
+// HornedBeast.js
 import React from 'react';
 
-function HornedBeast({ title, imageUrl, description }) {
+function HornedBeast(props) {
   return (
     <div>
-      <h2>{title}</h2>
-      <img src={`https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80} alt={title} title={title} `}/>
-      <p>{description}</p>
+      <h2>{props.title}</h2>
+      <img src={props.imageUrl} alt={props.title} title={props.title} />
+      <p>{props.description}</p>
     </div>
   );
 }
